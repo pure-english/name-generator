73e84332833eaa7a4bd33ba8ae0e1bacca293c3f
kaubu <29447633+kaubu@users.noreply.github.com>
2023-12-01 13:37:35 +0000

    v1.3.6 - add version number

--- src/views/Generator.vue
+++ src/views/Generator.vue
@@ -8,7 +8,7 @@
   <div class="container mx-auto">
     <div class="prose">
       <h1 class="grow my-5">Anglish Name Generator</h1>
-      <sub class="grow"><center>v1.3.5 (2023-12-01)</center></sub>
+      <sub class="grow"><center>v1.3.6 (2023-12-01)</center></sub>
     </div>
 
     <div class="content-stretch mt-8" style="max-width: 20%; min-width: 265px;">
